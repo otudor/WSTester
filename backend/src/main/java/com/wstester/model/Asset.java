1ac0625422c5b5049a48a48ed5c695be2ca69ff7
malexe <malexe@EN63219.endava.net>
2014-08-01 16:11:34 +0300

    Added name, type, path, last modified for Asset.java

--- backend/src/main/java/com/wstester/model/Asset.java
+++ backend/src/main/java/com/wstester/model/Asset.java
@@ -7,17 +7,53 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class Asset {
 	private String uuid;
+	private long lastmodified;
+	private String name;
+	private String path;
+	private String type;
 
 	public Asset() {
 		uuid = UUID.randomUUID().toString();
+	}
+
+	public String getUuid() {
+		return uuid;
 	}
 
 	public String getID() {
 		return this.uuid;
 	}
 
-	@Override
-	public boolean equals(Object obj) {
-		return true;
+	public long getLastmodified() {
+		return lastmodified;
 	}
+
+	public void setLastmodified(long lastmodified) {
+		this.lastmodified = lastmodified;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
 }
