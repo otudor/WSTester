489cce0a7ac0887f33eb93911fe2182202ba421b
aistoica <andrei.stoica@endava.com>
2014-08-22 09:58:49 +0300

    Removed the rest client Removed unused dependencies from the POM Added filters to the RouteDispatcher

--- backend/src/main/java/com/wstester/dispatcher/RouteDispatcher.java
+++ backend/src/main/java/com/wstester/dispatcher/RouteDispatcher.java
@@ -2,13 +2,20 @@ package com.wstester.dispatcher;
 
 import org.apache.camel.builder.RouteBuilder;
 
+import com.wstester.model.MongoStep;
+import com.wstester.model.RestStep;
+
 public class RouteDispatcher extends RouteBuilder{
 	
 	@Override
 	public void configure() throws Exception {
 	    
 		from("jms:startQueue")
-				.to("jms:restQueue");
+		.choice()
+			.when(body().isInstanceOf(RestStep.class))
+				.to("jms:restQueue")
+			.when(body().isInstanceOf(MongoStep.class))
+				.to("jms:mongoQueue");
 		
 	}
 }
--- backend/src/test/java/com/wstester/camel/TestBaseClass.java
+++ backend/src/test/java/com/wstester/camel/TestBaseClass.java
@@ -3,14 +3,11 @@ package com.wstester.camel;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-
-import com.wstester.client.rest.RestClient;
 import com.wstester.server.Main;
 
 public class TestBaseClass {
 
 	private static HttpServer server;
-	protected static RestClient client;
 	
     @BeforeClass
     public static void setUp() throws Exception {
