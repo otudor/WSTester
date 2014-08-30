31da6e38d3625c385dd9e8a32b8cad54363a0854
andrei <andrei.trandafir@endava.com>
2014-08-30 09:30:09 +0300

    andrei test

--- frontend/src/main/java/com/wstester/EnvironmentDetailPresenter.java
+++ frontend/src/main/java/com/wstester/EnvironmentDetailPresenter.java
@@ -6,34 +6,29 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.TextField;
 
-public class EnvironmentDetailPresenter
-{
-    @FXML private Node rootEnvDetails;
-    @FXML private TextField envNameField;
+public class EnvironmentDetailPresenter {
+	@FXML private Node rootEnvDetails;
+	@FXML private TextField envNameField;
 
-    private EnvironmentService environmentService;
-    private MainPresenter mainPresenter;
+	private EnvironmentService environmentService;
+	private MainPresenter mainPresenter;
 
-    public void setEnvironmentService(EnvironmentService environmentService)
-    {
-        this.environmentService = environmentService;
-    }
+	public void setEnvironmentService(EnvironmentService environmentService)	{
+		this.environmentService = environmentService;
+	}
 
-    public void setMainPresenter(MainPresenter mainPresenter)
-    {
-        this.mainPresenter = mainPresenter;
-    }
+	public void setMainPresenter(MainPresenter mainPresenter)	{
+		this.mainPresenter = mainPresenter;
+	}
 
-    public Node getView()
-    {
-        return rootEnvDetails;
-    }
-    
-    public void setEnvironment(final String envUID)
-    {
-        envNameField.setText("");
-        
-        Environment env = environmentService.getEnvironment( envUID);
-        envNameField.setText( env.getName());
-    }
+	public Node getView()	{
+		return rootEnvDetails;
+	}
+
+	public void setEnvironment(final String envUID)	{
+		envNameField.setText("");
+
+		Environment env = environmentService.getEnvironment( envUID);
+		envNameField.setText( env.getName());
+	}
 }
