dc64c2188b33c1e0ee980c3c1c2e9b1dfd209fb1
aistoica <andrei.stoica@endava.com>
2014-09-01 17:05:56 +0300

    Delayer for soap route

--- backend/src/main/java/com/wstester/dispatcher/SoapRoute.java
+++ backend/src/main/java/com/wstester/dispatcher/SoapRoute.java
@@ -4,6 +4,7 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+
 import com.wstester.model.Response;
 import com.wstester.model.SoapStep;
 
@@ -16,6 +17,7 @@ public class SoapRoute extends RouteBuilder {
 		
 		from("jms:soapQueue")
 		.bean(SoapConnection.class)
+		.delay().method(ExchangeDelayer.class, "delay").asyncDelayed()
 		.process(new Processor() {
 			
 			@Override
