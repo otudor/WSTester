a379bbeca3920aa7f48e3f7429cbf4983a548c46
StefanDinescu <stefan.dinescu@endava.com>
2014-09-11 10:43:50 +0300

    Implemented History Table in mySQLStep window

--- frontend/src/main/java/com/wstester/testFactory/MySQLStepController.java
+++ frontend/src/main/java/com/wstester/testFactory/MySQLStepController.java
@@ -12,6 +12,9 @@ import com.wstester.model.Step;
 import com.wstester.model.Execution;
 import com.wstester.model.StepStatusType;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
@@ -30,14 +33,15 @@ public class MySQLStepController
     @FXML private Label lblSQL;
     @FXML private Label lblStatus;
     @FXML private Label lblResponse;
-    @FXML private TableView<Execution> tblExecutions;
-    @FXML private TableColumn<Execution, String> columnDate;
-    @FXML private TableColumn<Execution, String> columnStatus;
-    @FXML private TableColumn<Execution, String> columnResponse;
+    @FXML private TableView<Execut> tblExecutions;
+    @FXML private TableColumn<Execut, String> columnDate;
+    @FXML private TableColumn<Execut, String> columnStatus;
+    @FXML private TableColumn<Execut, String> columnResponse;
     
     private MySQLStep step;    
     private TestSuiteService tsService;
     private TestSuiteManagerController tsMainController;
+    final ObservableList<Execut> lista = FXCollections.observableArrayList();
     
 	@FXML
 	private void initialize() 
@@ -60,7 +64,6 @@ public class MySQLStepController
         return rootMySQLStep;
     }
 
-
     public void setMySQLStep(final String stepUID)
     {
     	lblName.setText("");
@@ -79,7 +82,57 @@ public class MySQLStepController
         	//else ....
         		//FAILED
         	lblResponse.setText(execution.getResponse().getContent());
+        	Execut exemplu = new Execut(execution.getRunDate().toString(),execution.getStatus().toString(),execution.getResponse().getContent());
+            lista.add(exemplu);
+            tblExecutions.setItems(lista);
+            columnDate.setCellValueFactory(
+            		new PropertyValueFactory<Execut,String>("Date")
+            		);
+            
+            columnStatus.setCellValueFactory(
+            		new PropertyValueFactory<Execut,String>("Status")
+            		);
+            columnResponse.setCellValueFactory(
+            		new PropertyValueFactory<Execut,String>("Response")
+            		);
+           
         }
+          
+        
+        
     }
-
+    public static class Execut{
+    	private final SimpleStringProperty date;
+    	private final SimpleStringProperty status;
+    	private final SimpleStringProperty response;
+		
+    	private Execut(String Date, String Status, String Response){
+    		this.date = new SimpleStringProperty(Date);
+    		this.status = new SimpleStringProperty(Status);
+    		this.response = new SimpleStringProperty(Response);
+     	}
+    	
+    	public String getDate(){
+    		return date.get();
+    	}
+    	public String getStatus(){
+    		return status.get();
+    	}
+    	public String getResponse(){
+    		return response.get();
+    	}
+    	public void setDate(String Date){
+    		date.set(Date);
+    	}
+    	public void setStatus(String Status){
+    		status.set(Status);
+    	}
+    	public void setResponse(String Response){
+    		response.set(Response);
+    	}
+    }
+    
+    
+    
+    
 }
