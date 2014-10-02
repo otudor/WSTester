9c990f4a9ededeb3e3dad0e62385f8f951c6267b
fadwow <vlaaad.dumitrache@yahoo.com>
2014-10-02 17:39:51 +0300

    Variables icon

--- frontend/src/main/java/com/wstester/variables/TableVariables.java
+++ frontend/src/main/java/com/wstester/variables/TableVariables.java
@@ -1,20 +1,36 @@
 package com.wstester.variables;
 
+import java.util.ArrayList;
+
+import com.wstester.model.Variable;
+
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class TableVariables {
 	private final StringProperty name;
+	private final StringProperty content;
 	
-	public TableVariables(String name){
+	public TableVariables(String name, String content){
 		this.name = new SimpleStringProperty(name);
+		this.content = new SimpleStringProperty(content);
 	}
 	
 	public StringProperty getName() {
 		return name;
 	}
 
+	public StringProperty getContent() {
+		return content;
+	}
+	
 	public StringProperty nameProperty(){
 		return this.name;
 	}
+	
+	public StringProperty contentProperty(){
+		return this.content;
+	}
+	
+	
 }
